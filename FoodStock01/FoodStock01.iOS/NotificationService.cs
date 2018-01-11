5308b84d8e8409221dee8bde0d58cbb2021f92e3
150333 <150333@jc-21.jp>
2018-01-11 10:01:03 +0900

    1

---
+++ FoodStock01/FoodStock01.iOS/NotificationService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+using UserNotifications;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FoodStock01.NotificationService))]
+
+namespace FoodStock01
+{
+    public class NotificationService : INotificationService
+    {
+        public void Regist()
+        {
+            //if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            //{
+            UNAuthorizationOptions types = UNAuthorizationOptions.Badge |
+                                            UNAuthorizationOptions.Sound |
+                                            UNAuthorizationOptions.Alert;
+
+            UNUserNotificationCenter.Current.RequestAuthorization(types, (granted, err) =>
+            {
+                if (err != null)
+                {
+                    System.Diagnostics.Debug.WriteLine(err.LocalizedFailureReason + System.Environment.NewLine + err.LocalizedDescription);
+                }
+                if (granted)
+                {
+
+                }
+            });
+            //}
+        }
+        public void On(string title, string subtitle, string body)
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(delegate
+            {
+                var content = new UNMutableNotificationContent();
+                content.Title = title;
+                content.Subtitle = subtitle;
+                content.Body = body;
+                content.Sound = UNNotificationSound.Default;
+
+                var trigger = UNTimeIntervalNotificationTrigger.CreateTrigger(5, false);
+
+                //日時を指定する場合は以下の情報を付与
+                NSDateComponents components = new NSDateComponents();
+                components.TimeZone = NSTimeZone.DefaultTimeZone;
+                components.Year = DateTime.Now.Year;
+                components.Month = DateTime.Now.Month;
+                components.Day = DateTime.Now.Day;
+                //components.Hour = _notifyDate.LocalDateTime.Hour;
+                components.Hour = 10;
+                //components.Minute = _notifyDate.LocalDateTime.Minute;
+                components.Minute = 30;
+                //components.Minute = ;
+                components.Second = 0;
+                var calendarTrigger = UNCalendarNotificationTrigger.CreateTrigger(components, false);
+
+                var requestID = "notifyKey";
+                content.UserInfo = NSDictionary.FromObjectAndKey(new NSString("notifyValue"), new NSString("notifyKey"));
+                var request = UNNotificationRequest.FromIdentifier(requestID, content, trigger);
+
+                //var request = UNNotificationRequest.FromIdentifier(requestID, content, calendarTrigger);
+
+                //UNUserNotificationCenter.Current.Delegate = new LocalNotificationCenterDelegate();
+
+                //ローカル通知を予約する
+                UNUserNotificationCenter.Current.AddNotificationRequest(request, (err) =>
+                {
+                    if (err != null)
+                    {
+                        //LogUtility.OutPutError(err.LocalizedFailureReason + System.Environment.NewLine + err.LocalizedDescription);
+                    }
+                });
+                UIApplication.SharedApplication.ApplicationIconBadgeNumber += 1; //アイコン上に表示するバッジの数値
+            });
+        }
+
+        public void Off()
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(delegate
+            {
+                UNUserNotificationCenter.Current.RemovePendingNotificationRequests(new string[] { "notifyKey" });
+
+                //UNUserNotificationCenter.Current.RemoveAllDeliveredNotifications();
+            });
+        }
+
+        public void return0()
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(delegate
+            {
+                UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
+            });
+        }
+    }
+}
---
+++ FoodStock01/FoodStock01/INotificationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStock01
+{
+    public interface INotificationService
+    {
+        //iOS用の登録
+        void Regist();
+
+        //通知する
+        void On(string title, string subTitle, string body);
+
+        //通知を解除する
+        void Off();
+
+        //0にする
+        void return0();
+    }
+}
